acdea9ab1672e7859c28aeb87e953d8d662cdf87
Onkar Madli <onkarmadli@gmail.com>
2021-10-01 14:37:52 +0530

    change nav text to black

--- istriangle.js
+++ istriangle.js
@@ -8,15 +8,22 @@ function calculateSumOfTraingles(angle1,angle2,angle3){
 }
 
 function isTraingle(angle1,angle2,angle3) {
-    const sumOfAngles = calculateSumOfTraingles(
-        Number(inputs[0].value),
-        Number(inputs[1].value),
-        Number(inputs[2].value));
-    if(sumOfAngles === 180){
-        outputEl.innerText = "Yay! The angles form a traingle.";
+    if(Number(inputs[0].value)<1 ||
+    Number(inputs[1].value)<1 ||
+    Number(inputs[2].value)<1){
+        outputEl.innerText="Please enter a valid positive angle"
     } else {
-        outputEl.innerText = "Oh Oh! The angles don't form a traingle.";
+        const sumOfAngles = calculateSumOfTraingles(
+            Number(inputs[0].value),
+            Number(inputs[1].value),
+            Number(inputs[2].value));
+        if(sumOfAngles === 180){
+            outputEl.innerText = "Yay! The angles form a traingle.";
+        } else {
+            outputEl.innerText = "Oh Oh! The angles don't form a traingle.";
+        }
     }
+    
 }
 
 isTraingleBtn.addEventListener('click',isTraingle);
